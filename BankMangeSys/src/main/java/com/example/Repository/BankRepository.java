1cf41f9d2740454e271e893435149bbeabb51d65
LavanyaKadadas <120641058+LavanyaKadadas@users.noreply.github.com>
2024-07-04 16:17:23 +0530

    Delete BankMangeSys directory

--- BankMangeSys/src/main/java/com/example/Repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repository;
-
-
-import com.example.entities.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Integer> {
-}
-
--- BankMangeSys/src/main/java/com/example/Repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Repository;
-
-import com.example.entities.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {}
